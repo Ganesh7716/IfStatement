afcdc9b6a4f5fdb20ecb80d44a9cbe3d1ec07c16
Ganesh7716 <ganeshnikam1408@gmail.com>
2022-10-04 02:54:26 +0530

    Add project files.

---
+++ IfStatement/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IfStatement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 2;
+            if(number < 5);
+            {
+                Console.WriteLine("{0} is less thsn 5", number);
+
+            }
+            
+                Console.WriteLine("this is statement is alwayes expected.");
+
+            
+        }
+    }
+}
